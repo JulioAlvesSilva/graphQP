7518999bfa093064f50eb907556616e1ab5ea1fe
Julio Cesar Alves da <aps.juliocs@gmail.com>
2024-07-30 14:41:36 -0300

    30-07-2024 2

--- server/conectDB.js
+++ server/conectDB.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
 async function conectaNaDatabase() {
+    const dadosConexao = process.env.MONGO_CONECT;
     try {
-        mongoose.connect("mongodb+srv://apsjuliocs:wUAmvYkRmQLpdfgY@project.yu6gk0x.mongodb.net/projectDados?retryWrites=true&w=majority&appName=project");
+        mongoose.connect(dadosConexao);
         return mongoose.connection;
     } catch (error) {
         console.error("Falha na conexão", error)
